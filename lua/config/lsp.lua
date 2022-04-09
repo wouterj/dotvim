2952bc455569edc03560a355b92c1500a7b2090b
Wouter de Jong <wouter@wouterj.nl>
2022-04-09 23:21:44 +0200

    Revert PHP LSP back to PHPactor

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,22 +3,28 @@ local lspconfig = require'lspconfig'
 -- Disable Diagnostcs globally
 vim.diagnostic.config({virtual_text = false})
 
-lspconfig.intelephense.setup{
-    on_attach = function(client)
-        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-        local opts = { noremap=true, silent=true }
-        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
-        buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
-        buf_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-        -- Use LSP as the handler for omnifunc.
-        --    See `:help omnifunc` and `:help ins-completion` for more information.
-        vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+local on_attach = function(client)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local opts = { noremap=true, silent=true }
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
+    buf_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<Leader>cc', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 
-        -- For plugins with an `on_attach` callback, call them here. For example:
-        -- require('completion').on_attach()
-    end
+    -- Use LSP as the handler for omnifunc.
+    vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+
+lspconfig.phpactor.setup{
+    init_options = {
+        ["language_server_completion.trim_leading_dollar"] = true,
+    },
+    on_attach = on_attach
 }
+-- lspconfig.intelephense.setup{
+--     on_attach = on_attach
+-- }
 
 lspconfig.ansiblels.setup{
     root_dir=lspconfig.util.find_git_ancestor
