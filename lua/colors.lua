318f9bafb1ae95bd4db2b3b772472ce1a93c4d5c
Wouter de Jong <wouter@wouterj.nl>
2023-02-14 23:42:34 +0000

    Deal with missing Nord colortheme

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,32 +1,36 @@
 -- TODO find the motivation to rewrite this custom code in lua
 vim.cmd([[
-function! ChangeBackground(bg)
-    if a:bg == "dark"
-        set background=dark
-        colorscheme nord
-    else
-        set background=light
-        colorscheme nord
-    endif
-endfunction
+try
+    function! ChangeBackground(bg)
+        if a:bg == "dark"
+            set background=dark
+            colorscheme nord
+        else
+            set background=light
+            colorscheme nord
+        endif
+    endfunction
 
-let s:bg = readfile($HOME . "/.local/share/nvim/.vimbg")[0]
-if s:bg == "dark"
-    call ChangeBackground("dark")
-else
-    call ChangeBackground("light")
-endif
-
-function! ToggleBackground()
-    let l:bg = readfile($HOME . "/.local/share/nvim/.vimbg")[0]
-
-    if l:bg == "dark"
-        call ChangeBackground("light")
-        call writefile(["light"], $HOME . "/.local/share/nvim/.vimbg")
-    else
+    let s:bg = readfile($HOME . "/.local/share/nvim/.vimbg")[0]
+    if s:bg == "dark"
         call ChangeBackground("dark")
-        call writefile(["dark"], $HOME . "/.local/share/nvim/.vimbg")
+    else
+        call ChangeBackground("light")
     endif
-endfunction
-map <Leader>b :call ToggleBackground()<CR>
+
+    function! ToggleBackground()
+        let l:bg = readfile($HOME . "/.local/share/nvim/.vimbg")[0]
+
+        if l:bg == "dark"
+            call ChangeBackground("light")
+            call writefile(["light"], $HOME . "/.local/share/nvim/.vimbg")
+        else
+            call ChangeBackground("dark")
+            call writefile(["dark"], $HOME . "/.local/share/nvim/.vimbg")
+        endif
+    endfunction
+    map <Leader>b :call ToggleBackground()<CR>
+catch /^Vim\%((\a\+)\)\=:E185/
+    " deal with it
+endtry
 ]])
